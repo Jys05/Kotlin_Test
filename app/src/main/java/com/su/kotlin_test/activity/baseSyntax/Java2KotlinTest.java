dbcbca811ff152cdf9c90dabbee71cb85aeb0f04
Mr.Su <694900549@qq.com>
2018-03-03 18:55:09 +0800

    创建Java To Kotlin的测试类

---
+++ app/src/main/java/com/su/kotlin_test/activity/baseSyntax/Java2KotlinTest.java
@@ -0,0 +1,30 @@
+package com.su.kotlin_test.activity.baseSyntax;
+
+import android.os.Bundle;
+
+import com.su.kotlin_test.frame.BaseActivity;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Mr.Su on 2018/3/3.
+ */
+
+public class Java2KotlinTest {
+
+    private void test() {
+        String[] ha = new String[]{"as", "sad"};
+
+
+        String[] ha2 = new String[]{};
+
+        int i = 0;
+        while (i < ha.length) {
+
+            i++;
+        }
+    }
+}
