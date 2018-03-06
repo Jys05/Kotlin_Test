3d17cbaf4b8301cbb93c9e3cd622f539ff3d3d93
Mr.Su <694900549@qq.com>
2018-03-06 22:48:05 +0800

    添加习惯用法

---
+++ app/src/main/java/com/su/kotlin_test/Java2KotlinTest.java
@@ -0,0 +1,9 @@
+package com.su.kotlin_test;
+
+/**
+ * Created by Mr.Su on 2018/3/6.
+ */
+
+public class Java2KotlinTest {
+
+}
---
+++ app/src/main/java/com/su/kotlin_test/activity/idioms/Customer.kt
@@ -0,0 +1,7 @@
+package com.su.kotlin_test.activity.idioms
+
+/**
+ * Created by Mr.Su on 2018/3/6.
+ */
+data class Customer(val name: String, val email: String) {
+}
